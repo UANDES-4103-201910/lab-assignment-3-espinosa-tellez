de0ad12dc228e634466203a4a621c7145c0b1eef
MEspinosaGH <mespinosa3@miuandes.cl>
2019-03-28 22:56:18 -0300

    fixed errors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket
-  has_many :ticket_order, through: :ticket
 
 	def most_tickets_sold
 		self.joins(:ticket_order).select("name").group(:event_id).order("COUNT(event_id) DESC").first
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ApplicationRecord
   has_many :ticket_order
   has_many :ticket, through: :ticket_order
 	def most_expensive_ticket_bought
-		self.joins(:ticket_order).select("amount").order(amount: :desc).first
+		self.ticket_order.select("amount").order(amount: :desc).first
 	end
 	
 	def most_expensive_ticket_bought_between(start_date, finish_date)
-		self.joins(:Ticket_Order).where("Ticket_Order.date >= ?", start_date).where("Ticket_Order.date <= ?", finish_date).select("amount").order(amount: :desc).first
+		self.ticket_order.where("ticket_orders.date >= ?", start_date).where("ticket_orders.date <= ?", finish_date).select("amount").order(amount: :desc).first
 	end
 
 	def last_event
-	self.joins(:Ticket_Order, :Ticket, :Event).select("event.name").order("ticket_order.date desc").first
+	self.ticket_order.event.select("events.name").order("ticket_orders.date desc").first
 	end
 end
